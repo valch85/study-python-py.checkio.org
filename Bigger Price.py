29966ea47bfda180b4455b9d133b8d010fcaa1e1
Valentyn Chubukin <val@valch.name>
2020-05-09 22:41:30 +0300

    add Bigger Price.py

--- Bigger Price.py
+++ Bigger Price.py
@@ -1,14 +1,13 @@
-
-
-
 def bigger_price(limit: int, data: list) -> list:
-    #print(limit)
-    b = sorted(data, key=data.get)
-    print(b)
-    for d in range(len(data)):
-        print(data[d]['price'])
 
+    return sorted(data, key=lambda i: i["price"], reverse=True)[0:limit]
 
+#    last_index = len(data) - 1
+#    for i in range(0, last_index):
+#        for j in range(0, last_index - i):
+#            if data[j]["price"] < data[j + 1]["price"]:
+#                data[j], data[j + 1] = data[j + 1], data[j]
+#    return data[:limit]
 
 bigger_price(2, [
         {"name": "bread", "price": 100},
@@ -24,4 +23,5 @@ bigger_price(1, [
         {"name": "pen", "price": 5},
         {"name": "whiteboard", "price": 170}
     ]) #== [{"name": "whiteboard", "price": 170}]
+
 
