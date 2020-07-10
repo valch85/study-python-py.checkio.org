c900cc9299f2cf49ecfdd6190dfe99e96fd5e015
Valentyn Chubukin <val@valch.name>
2020-07-10 23:59:22 +0300

    Acceptable Password II

---
+++ Acceptable Password II.py
@@ -0,0 +1,44 @@
+# Taken from mission Acceptable Password I
+import re
+
+
+def is_acceptable_password(password: str) -> bool:
+    if len(password) > 6:
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def is_acceptable_password(password: str) -> bool:
+    if len(password) > 6 and has_didgit(password):
+        return True
+    else:
+        return False
+
+
+def has_didgit(password):
+    return bool(re.search(r'\d', password))
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('muchlonger') == False
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
