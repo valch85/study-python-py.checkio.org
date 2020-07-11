75920bc2472b3365518e18cdd0ff66d59d1ec06f
Valentyn Chubukin <val@valch.name>
2020-07-12 00:53:32 +0300

    Acceptable Password V

--- Acceptable Password V.py
+++ Acceptable Password V.py
@@ -1,3 +1,5 @@
+# Taken from mission Acceptable Password IV
+
 # Taken from mission Acceptable Password III
 
 # Taken from mission Acceptable Password II
@@ -72,6 +74,34 @@ if __name__ == '__main__':
 def is_acceptable_password(password: str) -> bool:
     if len(password) > 9:
         return True
+    elif len(password) > 6 and any(c.isdigit() for c in password) and not all(c.isdigit() for c in password) and any(
+            c.lower().islower() for c in password):
+        return True
+    else:
+        return False
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(is_acceptable_password('short'))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert is_acceptable_password('short') == False
+    assert is_acceptable_password('short54') == True
+    assert is_acceptable_password('muchlonger') == True
+    assert is_acceptable_password('ashort') == False
+    assert is_acceptable_password('muchlonger5') == True
+    assert is_acceptable_password('sh5') == False
+    assert is_acceptable_password('1234567') == False
+    assert is_acceptable_password('12345678910') == True
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
+
+def is_acceptable_password(password: str) -> bool:
+    if "password" in password.lower():
+        return False
+    if len(password) > 9:
+        return True
     elif len(password) > 6 and any(c.isdigit() for c in password) and any(c.lower().islower() for c in password):
         return True
     else:
@@ -91,4 +121,7 @@ if __name__ == '__main__':
     assert is_acceptable_password('sh5') == False
     assert is_acceptable_password('1234567') == False
     assert is_acceptable_password('12345678910') == True
+    assert is_acceptable_password('password12345') == False
+    assert is_acceptable_password('PASSWORD12345') == False
+    assert is_acceptable_password('pass1234word') == True
     print("Coding complete? Click 'Check' to earn cool rewards!")
