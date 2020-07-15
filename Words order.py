7788070be1d07bb4b08ed897ec9a868b380e5dbb
Valentyn Chubukin <val@valch.name>
2020-07-15 21:32:14 +0300

    add Words order.py

---
+++ Words order.py
@@ -0,0 +1,26 @@
+def words_order(text: str, words: list) -> bool:
+    text_words = {w for w in text.split() if w in words}
+    return list(sorted(text_words, key=text.index)) == words
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(words_order('hi world im here', ['world', 'here']))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert words_order('hi world im here', ['world', 'here']) == True
+    assert words_order('hi world im here', ['here', 'world']) == False
+    assert words_order('hi world im here', ['world']) == True
+    assert words_order('hi world im here',
+ ['world', 'here', 'hi']) == False
+    assert words_order('hi world im here',
+ ['world', 'im', 'here']) == True
+    assert words_order('hi world im here',
+ ['world', 'hi', 'here']) == False
+    assert words_order('hi world im here', ['world', 'world']) == False
+    assert words_order('hi world im here',
+ ['country', 'world']) == False
+    assert words_order('hi world im here', ['wo', 'rld']) == False
+    assert words_order('', ['world', 'here']) == False
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
