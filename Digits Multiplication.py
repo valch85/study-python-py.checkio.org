4d1918ab11aab1f787748a50aadd44e24ff07a37
Valentyn Chubukin <val@valch.name>
2020-08-12 23:24:32 +0300

    add conditions Digits Multiplication.py

---
+++ Digits Multiplication.py
@@ -0,0 +1,15 @@
+def checkio(number: int) -> int:
+    return 1
+
+
+#if __name__ == '__main__':
+#    print('Example:')
+#    print(checkio(123405))
+
+
+checkio(123405) #== 120
+checkio(999) #== 729
+checkio(1000) #== 1
+checkio(1111) #== 1
+
+
