32502cb583d2a8b8c476310f0d075ff839da3894
Valentyn Chubukin <val@valch.name>
2020-08-12 23:43:14 +0300

    solution Digits Multiplication.py

--- Digits Multiplication.py
+++ Digits Multiplication.py
@@ -1,15 +1,21 @@
 def checkio(number: int) -> int:
-    return 1
+    answer = 1
+    for digit in str(number):
+        digit = int(digit)
+        if digit == 0:
+            pass
+        else:
+            answer = answer * digit
+    return answer
 
 
-#if __name__ == '__main__':
-#    print('Example:')
-#    print(checkio(123405))
+if __name__ == '__main__':
+    print('Example:')
+    print(checkio(123405))
 
-
-checkio(123405) #== 120
-checkio(999) #== 729
-checkio(1000) #== 1
-checkio(1111) #== 1
-
-
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert checkio(123405) == 120
+    assert checkio(999) == 729
+    assert checkio(1000) == 1
+    assert checkio(1111) == 1
+    print("Coding complete? Click 'Check' to review your tests and earn cool rewards!")
