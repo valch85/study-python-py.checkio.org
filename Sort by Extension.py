e30efc1dd94e42108ed604d866d1679af3315fd2
Valentyn Chubukin <val@valch.name>
2020-08-12 23:20:28 +0300

    final commit Sort by Extension.py

--- Sort by Extension.py
+++ Sort by Extension.py
@@ -14,10 +14,11 @@ if __name__ == '__main__':
     print(sort_by_ext(['1.cad', '1.bat', '1.aa']))
 
     # These "asserts" are used for self-checking and not for an auto-testing
-sort_by_ext(['1.cad', '1.bat', '1.aa']) # result == ['1.aa', '1.bat', '1.cad']
-sort_by_ext(['1.cad', '1.bat', '1.aa', '2.bat']) # result == ['1.aa', '1.bat', '2.bat', '1.cad']
-sort_by_ext(['1.cad', '1.bat', '1.aa', '.bat']) # result == ['.bat', '1.aa', '1.bat', '1.cad']
-sort_by_ext(['1.cad', '1.bat', '.aa', '.bat']) # result == ['.aa', '.bat', '1.bat', '1.cad']
-sort_by_ext(['1.cad', '1.', '1.aa']) # result == ['1.', '1.aa', '1.cad']
-sort_by_ext(['1.cad', '1.bat', '1.aa', '1.aa.doc']) # result == ['1.aa', '1.bat', '1.cad', '1.aa.doc']
-sort_by_ext(['1.cad', '1.bat', '1.aa', '.aa.doc']) # result == ['1.aa', '1.bat', '1.cad', '.aa.doc']
+    assert sort_by_ext(['1.cad', '1.bat', '1.aa']) == ['1.aa', '1.bat', '1.cad']
+    assert sort_by_ext(['1.cad', '1.bat', '1.aa', '2.bat']) == ['1.aa', '1.bat', '2.bat', '1.cad']
+    assert sort_by_ext(['1.cad', '1.bat', '1.aa', '.bat']) == ['.bat', '1.aa', '1.bat', '1.cad']
+    assert sort_by_ext(['1.cad', '1.bat', '.aa', '.bat']) == ['.aa', '.bat', '1.bat', '1.cad']
+    assert sort_by_ext(['1.cad', '1.', '1.aa']) == ['1.', '1.aa', '1.cad']
+    assert sort_by_ext(['1.cad', '1.bat', '1.aa', '1.aa.doc']) == ['1.aa', '1.bat', '1.cad', '1.aa.doc']
+    assert sort_by_ext(['1.cad', '1.bat', '1.aa', '.aa.doc']) == ['1.aa', '1.bat', '1.cad', '.aa.doc']
+    print("Coding complete? Click 'Check' to earn cool rewards!")
