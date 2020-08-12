0956cf7bad72e9577317849f7306f42c2a657279
Valentyn Chubukin <val@valch.name>
2020-08-12 23:45:25 +0300

    init Number Length.py

---
+++ Number Length.py
@@ -0,0 +1,15 @@
+def number_length(a: int) -> int:
+    # your code here
+    return None
+
+
+#if __name__ == '__main__':
+#    print("Example:")
+#    print(number_length(10))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+number_length(10) == 2
+number_length(0) == 1
+number_length(4) == 1
+number_length(44) == 2
+
