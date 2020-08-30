cb06bc8c10f6a9afb59171f7fd16d55b8b2f24da
Valentyn Chubukin <val@valch.name>
2020-08-30 18:59:22 +0300

    some bare solution Between Markers.py

--- Between Markers.py
+++ Between Markers.py
@@ -1,10 +1,29 @@
+import re
 def between_markers(text: str, begin: str, end: str) -> str:
-    """
-        returns substring between two given markers
-    """
-    # your code here
-    return ''
-
+    print("")
+    print("#####")
+    print(text)
+    if begin in text:
+        start = text.find(begin) + len(begin)
+    else:
+        start = -1
+    ending = text.find(end)
+    print(start)
+    print(ending)
+    if ending > 0 and start > ending:
+        print("")
+    elif start < 0 and ending < 0:
+        print("answer")
+        print(text)
+    elif start < 0:
+        print("answer")
+        print(text[0:ending])
+    elif ending < 0:
+        print("answer")
+        print(text[start:])
+    else:
+        print(text[start:ending])
+    #return ''
 # These "asserts" are used for self-checking and not for testing
 between_markers('What is >apple<', '>', '<') #== "apple", "One sym"
 between_markers("<head><title>My new site</title></head>", "<title>", "</title>") #== "My new site", "HTML"
