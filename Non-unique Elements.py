dd3d201e464826188d3575af0f2f3f89122218cc
Valentyn Chubukin <val@valch.name>
2020-09-24 00:49:12 +0300

    solution for Non-unique Elements.py

--- Non-unique Elements.py
+++ Non-unique Elements.py
@@ -5,9 +5,10 @@ def checkio(data: list) -> list:
             new_list.append(iterator)
     return new_list
 
-list(checkio([1, 2, 3, 1, 3])) #== [1, 3, 1, 3], "1st example"
-list(checkio([1, 2, 3, 4, 5])) #== [], "2nd example"
-list(checkio([5, 5, 5, 5, 5])) #== [5, 5, 5, 5, 5], "3rd example"
-list(checkio([10, 9, 10, 10, 9, 8])) #== [10, 9, 10, 10, 9], "4th example"
 
-
+if __name__ == "__main__":
+    assert list(checkio([1, 2, 3, 1, 3])) == [1, 3, 1, 3], "1st example"
+    assert list(checkio([1, 2, 3, 4, 5])) == [], "2nd example"
+    assert list(checkio([5, 5, 5, 5, 5])) == [5, 5, 5, 5, 5], "3rd example"
+    assert list(checkio([10, 9, 10, 10, 9, 8])) == [10, 9, 10, 10, 9], "4th example"
+    print("It is all good. Let's check it now")
