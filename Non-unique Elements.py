c3897b9561d30138fa5d6cf7cfa261624030aab7
Valentyn Chubukin <val@valch.name>
2020-09-24 00:48:29 +0300

    works Non-unique Elements.py

--- Non-unique Elements.py
+++ Non-unique Elements.py
@@ -1,19 +1,9 @@
-from collections import Counter
-
 def checkio(data: list) -> list:
-    unique_list = []
-
-    for x in data:
-        # check if exists in unique_list or not
-        if x not in unique_list:
-            unique_list.append(x)
-            # print list
-    if list.count(unique_list) == 0:
-        print
-    else:
-        print(unique_list)
-        #return data
-    #print(data)
+    new_list = []
+    for iterator in data:
+        if data.count(iterator) > 1:
+            new_list.append(iterator)
+    return new_list
 
 list(checkio([1, 2, 3, 1, 3])) #== [1, 3, 1, 3], "1st example"
 list(checkio([1, 2, 3, 4, 5])) #== [], "2nd example"
