6b073cdc3c829307f14ca629b3c599fcf77283f0
Valentyn Chubukin <val@valch.name>
2020-09-25 22:30:10 +0300

    answer Second index.py

--- Second index.py
+++ Second index.py
@@ -1,12 +1,23 @@
 def second_index(text: str, symbol: str) -> [int, None]:
+    first_index = text.find(symbol)
+    if first_index == -1:
+        return None
+    else:
+        answer = text.find(symbol, first_index + 1)
+        if answer == -1:
+            return None
+        else:
+            return answer
 
-    # your code here
-    return 0
 
+if __name__ == '__main__':
+    print('Example:')
+    print(second_index("sims", "s"))
 
-
-second_index("sims", "s") #== 3, "First"
-second_index("find the river", "e") #== 12, "Second"
-second_index("hi", " ") #is None, "Third"
-second_index("hi mayor", " ") #is None, "Fourth"
-second_index("hi mr Mayor", " ") #== 5, "Fifth"
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert second_index("sims", "s") == 3, "First"
+    assert second_index("find the river", "e") == 12, "Second"
+    assert second_index("hi", " ") is None, "Third"
+    assert second_index("hi mayor", " ") is None, "Fourth"
+    assert second_index("hi mr Mayor", " ") == 5, "Fifth"
+    print('You are awesome! All tests are done! Go Check it!')
