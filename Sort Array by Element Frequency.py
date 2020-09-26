743c2ea6cf538f3a8bc364c675f6ad7c3ba52d29
Valentyn Chubukin <val@valch.name>
2020-09-27 02:11:09 +0300

    answer Sort Array by Element Frequency.py

--- Sort Array by Element Frequency.py
+++ Sort Array by Element Frequency.py
@@ -1,30 +1,40 @@
+# function that convert var to integer if possible
+def isint(x):
+    try:
+        a = int(x)
+    except ValueError:
+        return x
+    else:
+        return int(a)
+
 def frequency_sort(items):
+    # temp dict
     temp_dict = {}
+    # count amount of items in list and add them to dict
     for iterator in items:
-        #print(str(iterator) + " = " + str(items.count(iterator)))
         temp_dict.update({str(iterator): items.count(iterator)})
 
-    #sort_dict = {k: v for k, v in sorted(temp_dict.items(), key=lambda item: item[1])}
+    # temp dict for sorted items
     sort_dict = {}
+    # going via KEY and VALUE of temp_dict and sort them in a reverse mode by VALUE
+    # to get KEY use lambda (get second item in a list of [key, value]
     for k, v in sorted(temp_dict.items(), key=lambda item: item[1], reverse=True):
+        # add result to the dict
         sort_dict.update({k: v})
 
+    # list for answer
     answer = []
-    for i in sort_dict:
+    # going throw KEYs of sort_dict
+    for key in sort_dict:
+        # get VALUE of the KEY
+        value = sort_dict[key]
+        # loop that add KEY as many times as in VALUE
+        for iterable in range(value):
+            # use function isint to convert digits to an integer
+            answer.append(isint(key))
 
-        for (sort_dict[i] < 0):
-            answer.append(i)
-            sort_dict[i]=sort_dict[i]-1
+    return answer
 
-    print(answer)
-
-
-
-
-
-    #print(temp_dict.items())
-    #print(sort_dict)
-    #return None
 
 frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]) #== [4, 4, 4, 4, 6, 6, 2, 2]
 frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob']) #== ['bob', 'bob', 'bob', 'carl', 'alex']
