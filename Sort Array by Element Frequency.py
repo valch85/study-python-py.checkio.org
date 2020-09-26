ac8e0a528c3e37e45022ea3465d19178d6d8c0bd
Valentyn Chubukin <val@valch.name>
2020-09-26 22:40:01 +0300

    sort by value Sort Array by Element Frequency.py

--- Sort Array by Element Frequency.py
+++ Sort Array by Element Frequency.py
@@ -1,8 +1,30 @@
 def frequency_sort(items):
+    temp_dict = {}
+    for iterator in items:
+        #print(str(iterator) + " = " + str(items.count(iterator)))
+        temp_dict.update({str(iterator): items.count(iterator)})
 
-    return None
+    #sort_dict = {k: v for k, v in sorted(temp_dict.items(), key=lambda item: item[1])}
+    sort_dict = {}
+    for k, v in sorted(temp_dict.items(), key=lambda item: item[1], reverse=True):
+        sort_dict.update({k: v})
+
+    answer = []
+    for i in sort_dict:
+
+        for (sort_dict[i] < 0):
+            answer.append(i)
+            sort_dict[i]=sort_dict[i]-1
+
+    print(answer)
 
 
+
+
+
+    #print(temp_dict.items())
+    #print(sort_dict)
+    #return None
 
 frequency_sort([4, 6, 2, 2, 6, 4, 4, 4]) #== [4, 4, 4, 4, 6, 6, 2, 2]
 frequency_sort(['bob', 'bob', 'carl', 'alex', 'bob']) #== ['bob', 'bob', 'bob', 'carl', 'alex']
