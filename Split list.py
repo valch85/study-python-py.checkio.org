225efe0a3bc5c707e56a81873b0fb052971bfef1
Valentyn Chubukin <val@valch.name>
2020-10-02 23:49:14 +0300

    init Split list.py again

--- Split list.py
+++ Split list.py
@@ -1,12 +1,11 @@
 def split_list(items: list) -> list:
-    # your code here
     return [items]
 
 
 
-split_list([1, 2, 3, 4, 5, 6]) == [[1, 2, 3], [4, 5, 6]]
-split_list([1, 2, 3]) == [[1, 2], [3]]
-split_list([1, 2, 3, 4, 5]) == [[1, 2, 3], [4, 5]]
-split_list([1]) == [[1], []]
-split_list([]) == [[], []]
+split_list([1, 2, 3, 4, 5, 6]) #== [[1, 2, 3], [4, 5, 6]]
+split_list([1, 2, 3]) #== [[1, 2], [3]]
+split_list([1, 2, 3, 4, 5]) #== [[1, 2, 3], [4, 5]]
+split_list([1]) #== [[1], []]
+split_list([]) #== [[], []]
 
