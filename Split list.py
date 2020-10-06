c026cbb610714ef7a606fdc88b140a1cfcfcf4af
Valentyn Chubukin <val@valch.name>
2020-10-06 22:10:52 +0300

    answer Split list.py

--- Split list.py
+++ Split list.py
@@ -1,19 +1,18 @@
 def split_list(items: list) -> list:
-    arr1 = []
-    arr2 = []
-    print(int(len(items)/2))
+    answer = []
+    middle = int(len(items)/2)
     if len(items)% 2 == 0:
-        i = 0
-        for v in range(0, int(len(items)/2)):
-            print(items[i])
-            i = i+1
+        answer.append(items[0:middle])
+        answer.append(items[middle:])
+    else:
+        answer.append(items[0:middle+1])
+        answer.append(items[middle+1:])
+    return answer
 
-            #arr1.append(i)
-    #print(arr1)
 
 split_list([1, 2, 3, 4, 5, 6]) #== [[1, 2, 3], [4, 5, 6]]
-"""split_list([1, 2, 3]) #== [[1, 2], [3]]
+split_list([1, 2, 3]) #== [[1, 2], [3]]
 split_list([1, 2, 3, 4, 5]) #== [[1, 2, 3], [4, 5]]
 split_list([1]) #== [[1], []]
-split_list([]) #== [[], []]"""
+split_list([]) #== [[], []]
 
