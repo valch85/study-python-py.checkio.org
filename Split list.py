457da434e7d97ac87c479711e3466bc653d3ee35
Valentyn Chubukin <val@valch.name>
2020-10-06 20:38:19 +0300

    some idea Split list.py

--- Split list.py
+++ Split list.py
@@ -1,14 +1,19 @@
 def split_list(items: list) -> list:
     arr1 = []
     arr2 = []
+    print(int(len(items)/2))
     if len(items)% 2 == 0:
-        for i in range(0, len(items)/2):
-            yield items[i:i]
-    print(items)
+        i = 0
+        for v in range(0, int(len(items)/2)):
+            print(items[i])
+            i = i+1
+
+            #arr1.append(i)
+    #print(arr1)
 
 split_list([1, 2, 3, 4, 5, 6]) #== [[1, 2, 3], [4, 5, 6]]
-split_list([1, 2, 3]) #== [[1, 2], [3]]
+"""split_list([1, 2, 3]) #== [[1, 2], [3]]
 split_list([1, 2, 3, 4, 5]) #== [[1, 2, 3], [4, 5]]
 split_list([1]) #== [[1], []]
-split_list([]) #== [[], []]
+split_list([]) #== [[], []]"""
 
