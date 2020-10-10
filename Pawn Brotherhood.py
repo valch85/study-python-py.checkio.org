eda55cd84809f85f930877ce700f7242eeb25b0e
Valentyn Chubukin <val@valch.name>
2020-10-11 01:45:44 +0300

    solution Pawn Brotherhood.py

--- Pawn Brotherhood.py
+++ Pawn Brotherhood.py
@@ -1,21 +1,24 @@
-"""def safe_pawns(pawns: set) -> int:
-    save = []
-    for v in pawns:
-        if v :
-            save.add(v)
-    return(save.count())
+def left(position: str):
+    return(chr(ord(position[0])-1)+chr(ord(position[1])-1))
+
+
+def right(position: set):
+    return(chr(ord(position[0])+1)+chr(ord(position[1])-1))
+
+
+def safe_pawns(pawns: set) -> int:
+    pawns = list(pawns)
+    save_pawns = 0
+    for position in pawns:
+        if left(position) in pawns:
+            save_pawns = save_pawns + 1
+        elif right(position) in pawns:
+            save_pawns = save_pawns + 1
+        else:
+            save_pawns = save_pawns + 0
+    return save_pawns
 
 
 safe_pawns({"b4", "d4", "f4", "c3", "e3", "g5", "d2"}) #== 6
 safe_pawns({"b4", "c4", "d4", "e4", "f4", "g4", "e5"}) #== 1
-"""
 
-
-def left(v: set):
-    test_arr = []
-    for n in v:
-        test_arr.append(chr(ord(n[0])-1))
-        test_arr.append(chr(ord(n[1])-1))
-    print(test_arr)
-
-left({"b4", "d4", "f4", "c3", "e3", "g5", "d2"})
