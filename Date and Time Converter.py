c6eed6b9c05caa22e526098d56c798cfc8e04de1
Valentyn Chubukin <val@valch.name>
2020-10-13 02:33:08 +0300

    init Date and Time Converter.py

---
+++ Date and Time Converter.py
@@ -0,0 +1,9 @@
+def date_time(time: str) -> str:
+    #replace this for solution
+    return time
+
+
+date_time("01.01.2000 00:00") #== "1 January 2000 year 0 hours 0 minutes", "Millenium"
+date_time("09.05.1945 06:30") #== "9 May 1945 year 6 hours 30 minutes", "Victory"
+date_time("20.11.1990 03:55") #== "20 November 1990 year 3 hours 55 minutes", "Somebody was born"
+
