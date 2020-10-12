0e12411d7e61f86ef095644571e8cc6e6385cd70
Valentyn Chubukin <val@valch.name>
2020-10-12 14:58:14 +0300

    solution Sun Angle.py

--- Sun Angle.py
+++ Sun Angle.py
@@ -1,9 +1,16 @@
 def sun_angle(time):
+    h, m = map(int, time.split(':'))
+    #One hour is 15 degrees (= 360 / 24) and one minute is 0.25 degrees (= 360 / (24 * 60))
+    angle = 15 * h + m * 0.25 - 90
+    #return angle if 0 <= angle <= 180 else "I don't see the sun!"
 
-    return time
+    if 0 <= angle <= 180:
+        return angle
+    else:
+        return "I don't see the sun!"
 
 
-sun_angle("07:00") #== 15
+sun_angle("07:10") #== 15
 sun_angle("01:23") #== "I don't see the sun!"
 
 
