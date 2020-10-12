38714694659908afbb7843f022e2784822231e95
Valentyn Chubukin <val@valch.name>
2020-10-13 00:44:29 +0300

    init All the same.py

---
+++ All the same.py
@@ -0,0 +1,14 @@
+from typing import List, Any
+
+
+def all_the_same(elements: List[Any]) -> bool:
+    # your code here
+    return True
+
+
+
+all_the_same([1, 1, 1]) #== True
+all_the_same([1, 2, 1]) #== False
+all_the_same(['a', 'a', 'a']) #== True
+all_the_same([]) #== True
+all_the_same([1]) #== True
