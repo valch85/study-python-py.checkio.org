9cd6118984be97be6d82713d8552b8f9374fe557
Valentyn Chubukin <val@valch.name>
2020-10-14 12:58:25 +0300

    add 2 and 3 solutions Date and Time Converter.py

--- Date and Time Converter.py
+++ Date and Time Converter.py
@@ -1,3 +1,5 @@
+"""
+# first solution
 from datetime import datetime
 
 
@@ -15,10 +17,34 @@ def date_time(time: str) -> str:
         mmm = " minutes"
 
     # convert to correct output month format
-    #date_correct = temp_date.strftime("%d %B %Y %H %M")
-    month_correct = temp_date.strftime("%B %Y")
+    #month_correct = temp_date.strftime("%B %Y")
     return(str(temp_date.day) + " " + month_correct + " year " + str(temp_date.hour) + hhh + str(temp_date.minute) + mmm)
+"""
 
+
+# second solution
+from datetime import datetime
+
+
+def date_time(time: str) -> str:
+    # convert string to datetime format
+    temp_date = datetime.strptime(time, "%d.%m.%Y %H:%M")
+    # convert to correct output month format
+    month_correct = temp_date.strftime("%-d %B %Y year %-H hour" + 's' * (temp_date.hour != 1) + ' %-M minute' + 's' * (temp_date.minute != 1))
+    return month_correct
+
+
+
+"""
+# third solution
+import time as tm
+
+
+def date_time(time: str) -> str:
+    time = tm.strptime(time, '%d.%m.%Y %H:%M')
+    timestring = tm.strftime('%-d %B %Y year %-H hour' + 's' * (time.tm_hour != 1) + ' %-M minute' + 's' * (time.tm_min != 1), time)
+    print(timestring)
+"""
 
 date_time("01.01.2000 00:00") #== "1 January 2000 year 0 hours 0 minutes", "Millenium"
 date_time("09.05.1945 06:30") #== "9 May 1945 year 6 hours 30 minutes", "Victory"
