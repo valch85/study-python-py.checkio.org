8db859efc59b265b5510d65f64582f6da9a06b64
Valentyn Chubukin <val@valch.name>
2020-10-14 12:42:54 +0300

    answer Date and Time Converter.py

--- Date and Time Converter.py
+++ Date and Time Converter.py
@@ -1,6 +1,23 @@
+from datetime import datetime
+
+
 def date_time(time: str) -> str:
-    #replace this for solution
-    return time
+    # convert string to datetime format
+    temp_date = datetime.strptime(time, "%d.%m.%Y %H:%M")
+
+    if temp_date.hour == 1:
+        hhh = " hour "
+    else:
+        hhh = " hours "
+    if temp_date.minute == 1:
+        mmm = " minute"
+    else:
+        mmm = " minutes"
+
+    # convert to correct output month format
+    #date_correct = temp_date.strftime("%d %B %Y %H %M")
+    month_correct = temp_date.strftime("%B %Y")
+    return(str(temp_date.day) + " " + month_correct + " year " + str(temp_date.hour) + hhh + str(temp_date.minute) + mmm)
 
 
 date_time("01.01.2000 00:00") #== "1 January 2000 year 0 hours 0 minutes", "Millenium"
