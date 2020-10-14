3dcefb75d662bc0bd8eebd2f1839bef0578290ce
Valentyn Chubukin <val@valch.name>
2020-10-14 12:43:20 +0300

    answer Date and Time Converter.py

--- Date and Time Converter.py
+++ Date and Time Converter.py
@@ -23,4 +23,3 @@ def date_time(time: str) -> str:
 date_time("01.01.2000 00:00") #== "1 January 2000 year 0 hours 0 minutes", "Millenium"
 date_time("09.05.1945 06:30") #== "9 May 1945 year 6 hours 30 minutes", "Victory"
 date_time("20.11.1990 03:55") #== "20 November 1990 year 3 hours 55 minutes", "Somebody was born"
-
