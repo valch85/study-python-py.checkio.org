f98ac12927c8116862173278153632560d5ebaae
Valentyn Chubukin <val@valch.name>
2020-10-18 01:52:26 +0300

    init All Upper I.py

---
+++ All Upper I.py
@@ -0,0 +1,9 @@
+def is_all_upper(text: str) -> bool:
+
+
+
+
+is_all_upper('ALL UPPER') #== True
+is_all_upper('all lower') #== False
+is_all_upper('mixed UPPER and lower') #== False
+is_all_upper('') #== True
