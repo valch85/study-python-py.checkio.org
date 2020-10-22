8b756a791919a52909e797b02554f00248932ac6
Valentyn Chubukin <val@valch.name>
2020-10-22 13:10:30 +0300

    solution Max Digit.py

---
+++ Max Digit.py
@@ -0,0 +1,20 @@
+def max_digit(number: int) -> int:
+    answer = 0
+    for i in str(number):
+        if answer <= int(i):
+            answer = int(i)
+    return answer
+
+
+if __name__ == '__main__':
+    print("Example:")
+    print(max_digit(0))
+
+    # These "asserts" are used for self-checking and not for an auto-testing
+    assert max_digit(0) == 0
+    assert max_digit(52) == 5
+    assert max_digit(634) == 6
+    assert max_digit(1) == 1
+    assert max_digit(10000) == 1
+    print("Coding complete? Click 'Check' to earn cool rewards!")
+
