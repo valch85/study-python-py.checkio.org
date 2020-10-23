628ae9ddb723e71100a265feef1394505a23f34f
Valentyn Chubukin <val@valch.name>
2020-10-23 20:53:53 +0300

    init Split Pairs.py

--- Split Pairs.py
+++ Split Pairs.py
@@ -3,14 +3,8 @@ def split_pairs(a):
     return None
 
 
-if __name__ == '__main__':
-    print("Example:")
-    print(list(split_pairs('abcd')))
-
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert list(split_pairs('abcd')) == ['ab', 'cd']
-    assert list(split_pairs('abc')) == ['ab', 'c_']
-    assert list(split_pairs('abcdf')) == ['ab', 'cd', 'f_']
-    assert list(split_pairs('a')) == ['a_']
-    assert list(split_pairs('')) == []
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+list(split_pairs('abcd')) #== ['ab', 'cd']
+list(split_pairs('abc')) #== ['ab', 'c_']
+list(split_pairs('abcdf')) #== ['ab', 'cd', 'f_']
+list(split_pairs('a')) #== ['a_']
+list(split_pairs('')) #== []
