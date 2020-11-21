2b46d605d5fc11f19b37fc7a3b522ac3752d7e76
Valentyn Chubukin <val@valch.name>
2020-11-21 16:42:06 +0200

    init Beginning Zeros

---
+++ Beginning Zeros.py
@@ -0,0 +1,12 @@
+def beginning_zeros(number: str) -> int:
+
+    return 0
+
+
+
+beginning_zeros('100') #== 0
+beginning_zeros('001') #== 2
+beginning_zeros('100100') #== 0
+beginning_zeros('001001') #== 2
+beginning_zeros('012345679') #== 1
+beginning_zeros('0000') #== 4
