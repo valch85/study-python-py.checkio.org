4b7f79b9839e8523a99e76e5137c0a03fcb437b3
Valentyn Chubukin <val@valch.name>
2020-12-08 23:03:23 +0200

    answer Is Even

---
+++ Is even.py
@@ -0,0 +1,10 @@
+def is_even(num: int) -> bool:
+    if num%2 == 0:
+        return True
+    else:
+        return False
+
+
+print(is_even(2)) #== True
+print(is_even(5)) #== False
+print(is_even(0)) #== True
