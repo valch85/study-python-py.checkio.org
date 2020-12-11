c1c8086f28e1bdc763a8f3d8a4ef837ea53b32a0
Valentyn Chubukin <val@valch.name>
2020-12-11 21:25:55 +0200

    answer Isometric strings.py

--- Isometric strings.py
+++ Isometric strings.py
@@ -1,16 +1,10 @@
 def isometric_strings(str1: str, str2: str) -> bool:
-    # your code here
-    return None
+    return len(list(zip(str1, str2))) == len(list(str1))
 
 
-if __name__ == '__main__':
-    print("Example:")
-    print(isometric_strings('add', 'egg'))
-
-    # These "asserts" are used for self-checking and not for an auto-testing
-    assert isometric_strings('add', 'egg') == True
-    assert isometric_strings('foo', 'bar') == False
-    assert isometric_strings('', '') == True
-    assert isometric_strings('all', 'all') == True
-    print("Coding complete? Click 'Check' to earn cool rewards!")
+print(isometric_strings('add', 'egg')) #== True
+print(isometric_strings('foo', 'bar')) #== False
+print(isometric_strings('', '')) #== True
+print(isometric_strings('all', 'all')) #== True
+print(isometric_strings('1334557', 'abqceeg'))
 
