096071efe552ab01f74383c4a00e9d2fd12f37a1
Valentyn Chubukin <val@valch.name>
2020-12-11 21:30:01 +0200

    answer Isometric strings.py

--- Isometric strings.py
+++ Isometric strings.py
@@ -1,6 +1,18 @@
 def isometric_strings(str1: str, str2: str) -> bool:
     return len(list(zip(str1, str2))) == len(list(str1))
 
+"""
+# 2nd solution
+def isometric_strings(str1: str, str2: str) -> bool:
+    d = dict()
+    for index, ch in enumerate(str1):
+       if ch in d.keys():
+           if d[ch] != str2[index]:
+               return False
+       else:
+            d[ch] = str2[index]
+    return True
+"""
 
 print(isometric_strings('add', 'egg')) #== True
 print(isometric_strings('foo', 'bar')) #== False
