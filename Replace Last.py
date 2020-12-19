b1c9c550b975ca86274431c9e8c96366c43405d2
Valentyn Chubukin <val@valch.name>
2020-12-19 02:55:52 +0200

    middle Replace Last.py

--- Replace Last.py
+++ Replace Last.py
@@ -1,11 +1,18 @@
 def replace_last(line: list) -> list:
+    if len(line) <= 1:
+        return line
+    else:
+        answer = []
+        for i in str(line[-1]):
+            answer.append(int(i))
+        for i in line[:-1]:
+            answer.append(i)
+        return answer
 
-    return None
 
 
-
-print(assert replace_last([2, 3, 4, 1])) # == [1, 2, 3, 4]
-print(assert replace_last([1, 2, 3, 4])) # == [4, 1, 2, 3]
-print(assert replace_last([1])) # == [1]
-print(assert replace_last([])) #== []
+print(replace_last([2, 3, 4, 1])) #== [1, 2, 3, 4]
+print(replace_last([1, 2, 3, 4])) #== [4, 1, 2, 3]
+print(replace_last([1])) #== [1]
+print(replace_last([])) #== []
 
