e54f42346ba1672873c98f19d8fb798a1831ba89
Valentyn Chubukin <val@valch.name>
2020-12-21 20:52:40 +0200

    answer Remove all after.py

--- Remove all after.py
+++ Remove all after.py
@@ -2,10 +2,22 @@ from typing import Iterable
 
 
 def remove_all_after(items: list, border: int) -> Iterable:
-
-    return items
-
-
+    # check is borders exists in list of items if no return whole list
+    if border in items:
+        # create a counter that will increase each item check
+        counter = 0
+        for item in items:
+            # counter increase
+            counter += 1
+            # when border is the same as item
+            if item == border:
+                # return items list from begging till the counter
+                # (position where the border is the same as item)
+                return items[:counter]
+            else:
+                pass
+    else:
+        return items
 
 
 print(list(remove_all_after([1, 2, 3, 4, 5], 3)))# == [1, 2, 3]
@@ -14,5 +26,3 @@ print(list(remove_all_after([1, 1, 2, 4, 2, 3, 4], 2)))# == [1, 1, 2]
 print(list(remove_all_after([1, 1, 5, 6, 7], 2)))# == [1, 1, 5, 6, 7]
 print(list(remove_all_after([], 0)))# == []
 print(list(remove_all_after([7, 7, 7, 7, 7, 7, 7, 7, 7], 7)))# == [7]
-
-
