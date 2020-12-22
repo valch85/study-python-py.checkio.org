501f9e8d3ab7cd555e9a5698cfbef122a52b416b
Valentyn Chubukin <val@valch.name>
2020-12-23 01:15:21 +0200

    init Frequency sorting.py

---
+++ Frequency sorting.py
@@ -0,0 +1,10 @@
+def frequency_sorting(numbers):
+    #replace this for solution
+    return numbers
+
+
+print(frequency_sorting([1, 2, 3, 4, 5]))# == [1, 2, 3, 4, 5], "Already sorted"
+print(frequency_sorting([3, 4, 11, 13, 11, 4, 4, 7, 3]))# == [4, 4, 4, 3, 3, 11, 11, 7, 13], "Not sorted"
+print(frequency_sorting([99, 99, 55, 55, 21, 21, 10, 10]))# == [10, 10, 21, 21, 55, 55, 99, 99], "Reversed"
+
+
