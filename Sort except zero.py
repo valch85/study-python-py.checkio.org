2549810a6b893c9449dda77070d3f2985928f6c0
Valentyn Chubukin <val@valch.name>
2020-12-23 01:11:57 +0200

    answer Sort except zero

--- Sort except zero.py
+++ Sort except zero.py
@@ -3,8 +3,22 @@ from typing import Iterable
 
 
 def except_zero(items: list) -> Iterable:
+    temp = sorted(items)
+    temp_arr = []
+    answer_arr = []
+    counter = 0
+    for i in temp:
+        if i != 0:
+            temp_arr.append(i)
 
-    return items
+    for i in items:
+        if i != 0:
+            answer_arr.append(temp_arr[counter])
+            counter += 1
+        else:
+            answer_arr.append(int(0))
+
+    return answer_arr
 
 
 print(list(except_zero([5, 3, 0, 0, 4, 1, 4, 0, 7])))# == [1, 3, 0, 0, 4, 4, 5, 0, 7]
