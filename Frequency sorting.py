767021b3d4e8793024319512400b77fc2b4c85dd
Valentyn Chubukin <val@valch.name>
2020-12-24 15:55:20 +0200

    init Frequency sorting.py

--- Frequency sorting.py
+++ Frequency sorting.py
@@ -1,5 +1,8 @@
+# Your mission is to sort the list by the frequency of numbers included in it.
+# If a few numbers have an equal frequency - they should be sorted according to their natural order.
+# For example: [5, 2, 4, 1, 1, 1, 3] ==> [1, 1, 1, 2, 3, 4, 5]
 def frequency_sorting(numbers):
-    #replace this for solution
+
     return numbers
 
 
